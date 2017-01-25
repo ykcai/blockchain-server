a855b7c9c9625e9ed7cd6730777d54eccb2cb533
Michael Cai <michaelcai@michaels-mbp.blr.can.ibm.com>
2017-01-25 12:16:56 -0500

    changed unzip_url

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ var options = 	{
 	},
 	chaincode:{
 		zip_url: 'https://github.com/ykcai/blockchain-example/archive/master.zip',
-		unzip_dir: 'chaincode',													//subdirectroy name of chaincode after unzipped
+		unzip_dir: 'blockchain-example/chaincode',													//subdirectroy name of chaincode after unzipped
 		git_url: 'http://gopkg.in/ykcai/blockchain-example.v1/chaincode',		//GO get http url
 	}
 }
