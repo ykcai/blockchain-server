841ae8fc3535771a83d8600815f4f9cad851dc15
Michael Cai <michaelcai@michaels-mbp.blr.can.ibm.com>
2017-02-22 16:42:08 -0500

    always deploy to same chain

--- app.js
+++ app.js
@@ -56,9 +56,9 @@ var options = 	{
 		zip_url: 'https://github.com/ykcai/blockchain-code/archive/v2.zip',
 		unzip_dir: 'blockchain-code-2/chaincode',													//subdirectroy name of chaincode after unzipped
 		git_url: 'http://gopkg.in/ykcai/blockchain-code.v2/chaincode',		//GO get http url
-	}
 		//hashed cc name from prev deployment, comment me out to always deploy, uncomment me when its already deployed to skip deploying again
 		deployed_name: '47eb79c33d12d28730bb2ef67578169ef3391072119b64e18e6f61407242c429ea7071ddaedddff0a58f8b36e32a29db0660e15d4dfb66108c07401d4cc4162e'
+	}
 }
 
 if(process.env.VCAP_SERVICES){
